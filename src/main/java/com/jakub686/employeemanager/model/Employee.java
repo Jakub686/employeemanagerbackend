a23d9eb55b6747fc5352df9d08890f39cda22d36
Jakub <jakub.bchyla@gmail.com>
2022-10-12 16:59:13 +0200

    Employee.java

--- src/main/java/com/jakub686/employeemanager/model/Employee.java
+++ src/main/java/com/jakub686/employeemanager/model/Employee.java
@@ -1,11 +1,29 @@
 package com.jakub686.employeemanager.model;
 
-public class Employee {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
 }
